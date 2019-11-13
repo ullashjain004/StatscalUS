b84706ea959d6dd1d5d001e1c626b83ba0757c74
Sneha Philip <snehaphilip989@gmail.com>
2019-11-13 14:20:20 -0500

    Task: Defined addition function

---
+++ Calculator/Calculator.py
@@ -0,0 +1,38 @@
+from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+from Calculator.Division import division
+from Calculator.Multiplication import multiply
+from Calculator.Square import square
+from Calculator.sqroot import sqroot
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiply(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = round(division(a, b), 7)
+        return self.result
+
+    def square(self, a):
+        self.result = square(a)
+        return self.result
+
+    def squareroot(self, a):
+        self.result = sqroot(a)
+        return self.result
+
