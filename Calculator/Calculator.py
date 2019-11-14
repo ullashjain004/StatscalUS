3b5c1b52ba303319d72dabe96e3773c0d7cfa07c
Sneha Philip <snehaphilip989@gmail.com>
2019-11-14 09:47:13 -0500

    Feature: Defined square function

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -3,8 +3,8 @@ from Calculator.Subtraction import subtraction
 from Calculator.Division import division
 from Calculator.Multiplication import multiply
 from Calculator.Square import square
-from Calculator.sqrt import sqroot
-
+from Calculator.sqroot import sqroot
+ 
 
 class Calculator:
     result = 0
@@ -12,25 +12,36 @@ class Calculator:
     def __init__(self):
         pass
 
-    def add(self, a, b):
-        self.result = addition(a, b) 
-        return self.result
 
     def subtract(self, a, b):
         self.result = subtraction(a, b)
         return self.result
 
-    def multiply(self, a, b):
-        self.result = multiply(a, b)
+
+
+    def add(self, a, b):
+        self.result = addition(a, b)
         return self.result
+
+
 
     def divide(self, a, b):
         self.result = round(division(a, b), 7)
         return self.result
 
+
+
     def square(self, a):
         self.result = square(a)
         return self.result
+
+
+
+    def multiply(self, a, b):
+        self.result = multiply(a, b)
+        return self.result
+
+
 
     def squareroot(self, a):
         self.result = sqroot(a)
